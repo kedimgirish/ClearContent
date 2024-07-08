3d47832b32a0155f55d1ef3050c4f49de1eb8573
kedimgirish <36363359+kedimgirish@users.noreply.github.com>
2024-07-08 20:59:07 +0200

    Update index.js
    Removed the dead code and commented code

--- src/index.js
+++ src/index.js
@@ -3,92 +3,55 @@ import './assets/style.css';
 export async function init() {
   miro.board.ui.on('icon:click', async () => {
 
-// Get all items from the board
-const items = await miro.board.get();
+  // Get all items from the board
+  const items = await miro.board.get();
 
+  //For all the shapes, remove the content
+  items.forEach((items) => {
+    switch (items.shape) {
+      case 'rectangle':
+      case 'star':
+      case 'round_rectangle':
+      case 'circle':
+      case 'triangle':
+      case 'rhombus':
+      case 'parallelogram':
+      case 'trapezoid':
+      case 'pentagon':
+      case 'hexagon':
+      case 'octagon':
+      case 'wedge_round_rectangle_callout':
+      case 'cloud':
+      case 'cross':
+      case 'can':
+      case 'right_arrow':
+      case 'left_arrow':
+      case 'left_right_arrow':
+      case 'left_brace':
+      case 'right_brace':
+        items.content = "";
+        items.sync();
+    }
+  });
 
-items.forEach((items) => {
-  switch (items.shape) {
-    case 'rectangle':
-    case 'star':
-    case 'round_rectangle':
-    case 'circle':
-    case 'triangle':
-    case 'rhombus':
-    case 'parallelogram':
-    case 'trapezoid':
-    case 'pentagon':
-    case 'hexagon':
-    case 'octagon':
-    case 'wedge_round_rectangle_callout':
-    case 'cloud':
-    case 'cross':
-    case 'can':
-    case 'right_arrow':
-    case 'left_arrow':
-    case 'left_right_arrow':
-    case 'left_brace':
-    case 'right_brace':
-      items.content = "";
-      items.sync();
-      ;
+  //Get all the sticky notes on the baord
+  let stickyNotes = items.filter((item) => item.type === 'sticky_note')
+
+  // Delete sticky note content
+  for (const stickyNote of stickyNotes) {
+    stickyNote.content = "";
+    stickyNote.sync(); 
+  }
+
+  //Get all the text notes on the board
+  let textNotes = items.filter((item) => item.type === 'text')
+
+  // Delete text note content
+  for (const textNote of textNotes) {
+    textNote.content = "";
+    textNote.sync(); 
   }
 });
-
-
-let stickyNotes = items.filter((item) => item.type === 'sticky_note')
-
-// Delete sticky note content
-for (const stickyNote of stickyNotes) 
-{
-  stickyNote.content = "";
-  stickyNote.sync(); 
-}
-
-
-let textNotes = items.filter((item) => item.type === 'text')
-
-// Delete sticky note content
-for (const textNote of textNotes) 
-{
-  textNote.content = "";
-  textNote.sync(); 
-}
-
-
-/* let selectedItems = await miro.board.getSelection()
-
-// Filter sticky notes from selected items
-let stickyNotes = selectedItems.filter((item) => item.type === 'sticky_note')
-
-let rectangles = selectedItems.filter((item) => item.shape === 'rectangle')
-
-let stars = selectedItems.filter((item) => item.shape === 'star')
-
-// Delete selected stickers
-for (const stickyNote of stickyNotes) 
-{
-  stickyNote.content = "";
-  stickyNote.sync(); 
-}
-
- //Delete selected stickers
-for (const rectangle of rectangles) 
-{
-  rectangle.content = "";
-  rectangle.sync(); 
-}
-
- //Delete selected stickers
- for (const star of stars) 
- {
-   star.content = "";
-   star.sync(); 
- } */
-
-
-
-  });
 }
 
 init();
